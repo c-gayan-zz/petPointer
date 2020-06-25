820b3ace13812e06480b4d596622560a5504e5ea
c-gayan <64426670+c-gayan@users.noreply.github.com>
2020-06-26 00:13:20 +0530

    Add files via upload

---
+++ Arduino code/maqiatto/Credentials.h
@@ -0,0 +1,21 @@
+#ifndef ARDUINO_CREDENTIALS_H
+#define ARDUINO_CREDENTIALS_H
+
+/* WiFi Credentials to connect Internet */
+#define STA_SSID "octo"
+#define STA_PASS "12345678"
+
+/* Provide MQTT broker credentials as denoted in maqiatto.com. */
+#define MQTT_BROKER       "maqiatto.com"
+#define MQTT_BROKER_PORT  1883
+#define MQTT_USERNAME     "cgayan10@gmail.com"
+#define MQTT_KEY          "pet123"
+
+
+/* Provide topic as it is denoted in your topic list. 
+ * For example mine is : cadominna@gmail.com/topic1
+ * To add topics, see https://www.maqiatto.com/configure
+ */
+#define TOPIC    "cgayan10@gmail.com/petpointer"
+
+#endif /* ARDUINO_CREDENTIALS_H */
---
+++ Arduino code/maqiatto/maqiatto.ino
@@ -0,0 +1,86 @@
+#include <ESP8266WiFi.h>
+#include <Arduino.h>
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+
+#include "MQTTConnector.h"
+#include "Credentials.h"
+
+#define WIFI_TIMEOUT 1000
+#define LOOP_TIMEOUT 50
+
+TinyGPSPlus gps;  // The TinyGPS++ object
+SoftwareSerial ss(4, 5); // The serial connection to the GPS device
+
+
+float latitude , longitude;
+int year , month , date, hour , minute , second;
+String date_str , time_str , lat_str , lng_str;
+int pm;
+
+void WiFiBegin(const char* ssid, const char* pass)
+{
+  WiFi.begin(ssid, pass);
+  Serial.printf("Waiting for AP connection ...\n");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(WIFI_TIMEOUT);
+    Serial.printf(".");
+  }
+  IPAddress ip = WiFi.localIP();
+  Serial.printf("\nConnected to AP. IP : %d.%d.%d.%d\n",
+                ip[0], ip[1], ip[2], ip[3]);
+}
+
+void setup()
+{
+  ss.begin(9600);
+  Serial.begin(9600);
+  Serial.setDebugOutput(true);
+  WiFiBegin(STA_SSID, STA_PASS);
+  MQTTBegin();
+}
+
+void loop()
+{
+
+  while (ss.available() > 0)
+    if (gps.encode(ss.read()))
+    {
+      if (gps.location.isValid())
+      {
+        latitude = gps.location.lat();
+        lat_str = String(latitude , 6);
+        longitude = gps.location.lng();
+        lng_str = String(longitude , 6);
+      }
+    }
+
+  delay(100);
+
+  String str = lat_str + "," + lng_str ;
+  
+  // Length (with one extra character for the null terminator)
+  int str_len = str.length() + 1;
+
+  // Prepare the character array (the buffer)
+  char char_array[str_len];
+
+  // Copy it over
+  str.toCharArray(char_array, str_len);
+    
+  Serial.print(char_array);
+
+ 
+
+ 
+  MQTTLoop();
+  if (MQTTPublish(TOPIC, char_array))
+  {
+    Serial.printf("MQTTPublish was succeeded.\n");
+  }
+  delay(LOOP_TIMEOUT);
+
+
+  
+}
